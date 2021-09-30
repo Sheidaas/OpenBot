8548c768eae629ce3f6b190d8fe21b9102cc72f6
Maciej Wrzeszcz <maciejwrzeszcz1@wp.pl>
2021-09-30 12:12:13 +0100

    fix

--- OpenBot/Modules/Schema/schema_runner_module.py
+++ OpenBot/Modules/Schema/schema_runner_module.py
@@ -16,6 +16,13 @@ class SchemaRunnerModule(ui.ScriptWindow):
         self.currRepeat = 0
         self.lastTime = 0
 
+    def DiscardCurrentSchema(self):
+        self.currSchema = None
+        self.currStage = 0
+        self.currAction = 0
+        self.isCurrActionDone = True
+        self.currRepeat = 0
+
     def SetIsCurrActionDoneTrue(self):
         self.GoToNextAction()
         self.isCurrActionDone = True
