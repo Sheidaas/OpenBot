e20b88b4b7731102fcc9d4fc1339e5da9b12e9ea
Maciej Wrzeszcz <maciejwrzeszcz1@wp.pl>
2021-09-30 12:40:57 +0100

    fix

--- OpenBot/Modules/Schema/schema_runner_interface.py
+++ OpenBot/Modules/Schema/schema_runner_interface.py
@@ -5,11 +5,9 @@ class SchemaRunnerInterface:
     def SetStatus(self, status):
         if status['Enabled'] != instance.enabled:
             self.SwitchEnabled()
-
         status_keys = status.keys()
         if 'ResetSchema' in status_keys:
             self.ResetSchema()
-
 
     def GetStatus(self):
         return {
@@ -19,7 +17,7 @@ class SchemaRunnerInterface:
             'CurrAction': instance.currAction,
             'CurrRepeat': instance.currRepeat
         }
-
+        
     def ResetSchema(self):
         instance.currStage = 0
         instance.currAction = 0
@@ -27,15 +25,24 @@ class SchemaRunnerInterface:
         instance.currRepeat = 0
 
     def SetNewSchema(self, schema):
+        instance.currStage = 0
+        instance.currAction = 0
+        instance.isCurrActionDone = True
         instance.currSchema = schema
     
     def DiscardCurrentSchema(self):
-        instance.DiscardCurrentSchema()
+        instance.currStage = 0
+        instance.currAction = 0
+        instance.isCurrActionDone = True
+        instance.currSchema = None
 
     def SwitchEnabled(self):
         if instance.enabled:
             instance.enabled = False
         else:
             instance.enabled = True
+        instance.currStage = 0
+        instance.currAction = 0
+        instance.isCurrActionDone = True
 
 schema_runner_interface = SchemaRunnerInterface()
