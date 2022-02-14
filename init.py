83a9e1839eea5b60a9261018c0958411e0ba5bff
Bloodiko <bastianjesuiter@gmail.com>
2022-02-14 21:41:26 +0100

    small QOL improvement

--- init.py
+++ init.py
@@ -1,13 +1,15 @@
-#Responsible for inputing the right methods
-#DON'T RUN SCRIPTS FROM ON HERE
+# Responsible for inputing the right methods
+# DON'T RUN SCRIPTS FROM ON HERE
 
-import sys, os
+import sys
+import os
 import chatm2g as _chat
 import playerm2g2 as _player
 import m2netm2g as _net
 import chrmgrm2g as _chrmgr
 import eXLib
-import chr,app
+import chr
+import app
 
 sys.modules['player'] = _player
 sys.modules['net'] = _net
@@ -15,11 +17,12 @@ sys.modules['chat'] = _chat
 sys.modules['chrmgr'] = _chrmgr
 
 
-def SetSingleDIKKeyState(key,state):
+def SetSingleDIKKeyState(key, state):
     if state == 1:
         _player.OnKeyDown(key)
     else:
         _player.OnKeyUp(key)
+
 
 def SetAttackKeyState(state):
     if state == 1:
@@ -27,15 +30,16 @@ def SetAttackKeyState(state):
     else:
         _player.OnKeyUp(app.DIK_SPACE)
 
+
 setattr(chr, 'GetPixelPosition', eXLib.GetPixelPosition)
 setattr(chr, 'MoveToDestPosition', eXLib.MoveToDestPosition)
 setattr(_net, 'GetMainActorVID', _player.GetMainCharacterIndex)
 setattr(_player, 'SetSingleDIKKeyState', SetSingleDIKKeyState)
 setattr(_player, 'SetAttackKeyState', SetAttackKeyState)
 
-#Adding paths to sys.path
+# Adding paths to sys.path
 
 sys.path.append(os.path.join(eXLib.PATH))
-sys.path.append(os.path.join(eXLib.PATH,'OpenBot'))
-sys.path.append(os.path.join(eXLib.PATH,'OpenBot', 'lib'))
-
+sys.path.append(os.path.join(eXLib.PATH, 'OpenBot'))
+sys.path.append(os.path.join(eXLib.PATH, 'OpenBot', 'lib'))
+sys.path.append(os.path.join(eXLib.PATH, 'OpenBot', 'Modules'))
